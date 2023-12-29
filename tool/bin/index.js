62f2d14bef4a2c1f42be385e87ef88084a412be7
jsmld <samvanerpersroijaards@gmail.com>
2023-12-29 16:41:27 -0300

    Adds error handling

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 const arg = require('arg')
 
-const args = arg({
-  '--start': Boolean,
-  '--build': Boolean
-})
-console.log(args)
+try {
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean
+  })
+
+  if (args['--start']) console.log('Starting app...')
+} catch (error) {
+  console.log(error.message)
+  console.log()
+  usage()
+}
+
+function usage () {
+  console.log(`tool [CMD]
+  --start\tStarts the app
+  --build\tBuilds the app`)
+}
